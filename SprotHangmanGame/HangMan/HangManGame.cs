33e99f50d33f59cf2024eb2671ea62dc3feb4aa2
Serj Prots <serganas995@gmail.com>
2022-04-13 21:43:16 +0300

    Добавьте файлы проекта.

---
+++ SprotHangmanGame/HangMan/HangManGame.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprotHangmanGame
+{
+    public class HangManGame
+    {
+        public enum GameStatus
+        {
+            Gameisunstarted,
+            Gameisunfinished,
+            Gameinprogress,
+            Won,
+            Lost
+        }
+        private readonly int AllowMisses;
+
+        private int TriesCounter { get; set; }
+
+        public int RemainingTies
+        {
+            get
+            {
+                return AllowMisses - TriesCounter;
+            }
+        }
+        public GameStatus gamestatus { get; set; } = GameStatus.Gameisunstarted;
+        public string Word { get; private set; }
+
+        public bool[] openindexes { get; private set; }
+
+        private string triedLetters;
+        public string TriedLetters
+        {
+            get
+            {
+                var chars = triedLetters.ToCharArray();
+                Array.Sort(chars);
+                return new string(chars);
+            }
+        }
+        public HangManGame(int AllowMisses = 6)
+        {
+            this.AllowMisses = AllowMisses;
+            TriesCounter = 0;
+        }
+
+        public string Setword()
+        {
+            string[] words = File.ReadAllLines("C:\\Users\\Dell\\source\\repos\\SprotHangmanGame\\SprotHangmanGame\\HangMan\\WordsStockRus.txt");
+            Random r = new Random();
+            int randomindex = r.Next(words.Length - 2);
+            Word = words[randomindex];
+
+            gamestatus = GameStatus.Gameinprogress;
+
+            openindexes = new bool[Word.Length];
+
+            return Word;
+        }
+
+        public string GuessLetter(char letter)
+        {
+            if (TriesCounter == AllowMisses)
+            {
+                throw new InvalidOperationException($"Exceeded the max misses number: {AllowMisses}");
+            }
+
+            if (gamestatus != GameStatus.Gameinprogress)
+            {
+                throw new InvalidOperationException($"Inaproppriate status of the game {gamestatus}");
+            }
+
+            bool openAny = false;
+            string result = string.Empty;
+            
+            for (int i = 0; i < Word.Length; i++)
+            {
+
+
+                if (Word[i] == letter)
+                {
+                    openindexes[i] = true;
+                    openAny = true;
+                }
+
+                if (openindexes[i])
+                {
+                    result +=Word[i];
+                }
+                else
+                {
+                    result += "-";
+                }
+            }
+            if (!openAny)
+            {
+                TriesCounter++;
+            }
+
+            triedLetters += letter;
+
+
+            if (isWin())
+            {
+                gamestatus = GameStatus.Won;
+
+            }
+            else if (TriesCounter == AllowMisses)
+            {
+                gamestatus = GameStatus.Lost;
+            }
+            return result;
+        }
+
+        private bool isWin()
+        {
+            foreach (var cur in openindexes)
+            {
+                if (cur == false)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
---
+++ SprotHangmanGame/HangMan/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprotHangmanGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           while (true)
+            {
+                HangManGame game = new HangManGame();
+                string word = game.Setword();
+                Console.WriteLine($"Guess the word!\nThe word is consist of {word.Length} - letters");
+                while (game.gamestatus == HangManGame.GameStatus.Gameinprogress)
+                {
+                    Console.WriteLine("Pick a letter");
+                    char s = Console.ReadLine().ToCharArray()[0];
+                    string curState = game.GuessLetter(s);
+                    Console.WriteLine(curState);
+
+                    Console.WriteLine($"Remaining tries = {game.RemainingTies}\nTriedLetters: {game.TriedLetters}");
+                }
+
+                if (game.gamestatus == HangManGame.GameStatus.Lost)
+                {
+                    Console.WriteLine($"You`re hanged...\nThe word was: {game.Word}");
+
+                }
+                else if (game.gamestatus == HangManGame.GameStatus.Won)
+                {
+                    Console.WriteLine("You won!");
+                }
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
